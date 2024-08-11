bc378e1cf6bb83af71f82f0ff428b62c086fbf9d
NandoCraninckx <craninckxnando@outlook.com>
2024-08-11 02:00:55 +0200

    Change texts in contact & add all project names

--- my-portfolio/src/components/contactInfo/ContactInfo.jsx
+++ my-portfolio/src/components/contactInfo/ContactInfo.jsx
@@ -20,23 +20,23 @@ const ContactInfo = () => {
 
   return (
     <section className="ContactInfo section" id="contact">
-        <h2 className="section-title">Get in touch</h2>
-        <span className="section-subtitle">Contact me</span>
+        <h2 className="section-title">Contacteer me</h2>
+        <span className="section-subtitle">Via één van onderstaande kanalen</span>
 
         <div className="contact-container container grid">
             <div className="contact-content">
-                <h3 className="contact-title">Talk to me</h3>
+                <h3 className="contact-title">Socials</h3>
             
 
             <div className="contact-info">
                 <div className="contact-card">
-                    <i className="bx bx-mail-send contact-card-icon"></i>
+                    <i className="bx bxl-whatsapp contact-card-icon"></i>
 
-                    <h3 className="contact-card-title">E-mail</h3>
+                    <h3 className="contact-card-title">WhatsApp</h3>
                     <span className="contact-card-data">0494/12 34 56</span>
 
                     <a href="" className="contact-button">
-                        Write me <i className="bx bx-right-arrow-alt
+                        Via WhatsApp <i className="bx bx-right-arrow-alt
                         contact-button-icon"></i>
                     </a>
                 </div>
@@ -48,7 +48,7 @@ const ContactInfo = () => {
                     <span className="contact-card-data">Nando Craninckx</span>
 
                     <a href="https://www.linkedin.com/in/nando-craninckx/" className="contact-button">
-                        Bekijk mijn LinkedIn-pagina <i className="bx bx-right-arrow-alt
+                        Via mijn LinkedIn-pagina <i className="bx bx-right-arrow-alt
                         contact-button-icon"></i>
                     </a>
                 </div>
@@ -60,7 +60,7 @@ const ContactInfo = () => {
                     <span className="contact-card-data">NandoCraninckx</span>
 
                     <a href="https://github.com/NandoCraninckx" className="contact-button">
-                        Bekijk mijn Github-account <i className="bx bx-right-arrow-alt
+                        Via mijn GitHub-account <i className="bx bx-right-arrow-alt
                         contact-button-icon"></i>
                     </a>
                 </div>
@@ -69,36 +69,36 @@ const ContactInfo = () => {
         </div>
 
             <div className="contact-content">
-                <h3 className="contact-title">Write me your project</h3>
+                <h3 className="contact-title">Stuur een e-mail</h3>
 
                 <form ref={form} onSubmit={sendEmail} className="contact-form">
                     <div className="contact-form-div">
-                        <label className="contact-form-tag">Mail</label>
+                        <label className="contact-form-tag">E-mail</label>
                         <input 
                             type="email" 
                             name="email" 
                             className="contact-form-input" 
-                            placeholder="Insert your email"
+                            placeholder="Vul je e-mail in"
                         />
                     </div>
 
                     <div className="contact-form-div">
-                        <label className="contact-form-tag">Name</label>
+                        <label className="contact-form-tag">Naam</label>
                         <input 
                             type="text" 
                             name="name" 
                             className="contact-form-input" 
-                            placeholder="Insert your name"
+                            placeholder="Geef je naam op"
                         />
                     </div>
 
                     <div className="contact-form-div contact-form-area">
-                        <label className="contact-form-tag">Project</label>
+                        <label className="contact-form-tag">Bericht</label>
                         <textarea 
                             name="message" 
                             cols="30" rows="10"
                             className="contact-form-input"
-                            placeholder="Write your message">   
+                            placeholder="Laat een bericht achter">   
                         </textarea>
                     </div>
 
--- my-portfolio/src/components/projecten/AccordionMenu.jsx
+++ my-portfolio/src/components/projecten/AccordionMenu.jsx
@@ -16,17 +16,17 @@ const AccordionMenu = () => {
                     <h1 className="accordion-title">Projecten </h1>
                     <div className="accordion-items-container">
                         <div className="accordion-items">
-                            <AccordionItem title="Website Portfolio"
+                            <AccordionItem title="Project 1: Portfolio Website"
                                 content="Dit is mijn portfolio website. Hier kan je al mijn projecten bekijken en meer te weten komen over mij."
                                 isActive={isActive === 1}
                                 onClick={() => handleClick(1)}/>
                                 
-                                <AccordionItem title="Project 2"
+                                <AccordionItem title="Project 2: RPG-game ontwikkeld met Godot"
                                 content="Hier wordt nog hard aan gewerkt."
                                 isActive={isActive === 2}
                                 onClick={() => handleClick(2)}/>
 
-                                <AccordionItem title="Project 3"
+                                <AccordionItem title="Project 3: Certified MuleSoft Developer"
                                 content="Hier wordt nog hard aan gewerkt."
                                 isActive={isActive === 3}
                                 onClick={() => handleClick(3)}/>
