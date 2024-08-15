dd2896401dd66608e129c9815f628299a8944a00
NandoCraninckx <craninckxnando@outlook.com>
2024-08-15 20:18:33 +0200

    Add eindreflectie

--- my-portfolio/src/components/reflectie/Reflectie.jsx
+++ my-portfolio/src/components/reflectie/Reflectie.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import "./Reflectie.css";
 
 const Reflectie = () => {
   return (
     <section className="reflectie section" id="reflectie">
       <div className="reflectie-container container grid">
                 <div className="reflectie-content grid">
-                    <h1 className="section-title">Reflectie</h1>
-                    
+                    <h1 className="section-title">Eindreflectie</h1>
+                    <p className="eindreflectie-paragraph">
+                    Aangekomen bij het einde van I-Talent kijk ik er over het algemeen positief op terug. 
+                    Op zich is dit opleidingsonderdeel een nuttig iets en draagt het bij aan de ontwikkeling van een toekomstig IT’er. 
+                    De IT-wereld is continu in verandering en als je niet actief iets onderneemt om bij te blijven, loop je veel kans om achterop te raken of opportuniteiten te missen. 
+                    Dit opleidingsonderdeel wil stimuleren dat je dit zeker wel doet en dat is een goed iets. Het moeilijkere kantje aan dit verhaal is dat je er ook effectief tijd voor moet vinden om deze projecten te kunnen maken. 
+                    Dit was in mijn persoonlijk geval een struikelblok en verplichtte mij er ook toe om dit opleidingsonderdeel pas in de herexamens te kunnen finaliseren. 
+                    </p>
+                    <p className="eindreflectie-paragraph">
+                    Deze zoektocht naar de balans tussen tijd voor de opleiding, mijn job en mijn gezin was voor mij een beetje de rode draad door deze drie jaar durende opleiding. 
+                    Aan de start was ik mij hiervan zeker bewust dat dit veel opoffering en doorzettingsvermogen zou vragen. Maar wauw, wat is me dit een huzarenstukje gebleken. 
+                    Het eerste jaar werd er met zeer veel goesting en goede moed gestart. Maar ik merkte aan mijzelf dat het per semester steeds moeilijker en pittiger werd om die motor aan de gang te houden. 
+                    Na lange werkdagen bij mijn vorige werkgever, waar ik ongemotiveerd zat op een uitgekeken job, mij ‘s avonds en ook vaak ‘s nachts terug motiveren om er voor school in te vliegen. 
+                    Weekends opofferen en vaak de ‘nee, ik moet passen want heb nog redelijk wat werk’ bovenhalen. Ik denk dat mijn omgeving die zin de afgelopen drie jaar vaak gehoord moet hebben. 
+                    Het werd voor mij nog een stukje zwaarder toen ik het laatste jaar hierbij nog de bouw van onze nieuwe woonst en de geboorte van mijn dochter moest combineren. 
+                    </p>
+                    <p className="eindreflectie-paragraph">
+                    Door dit alles ben ik mijn eigen wel beter leren kennen. Het belangrijkste is dat ik over meer doorzettingsvermogen beschik dan ik initieel wist. 
+                    Verder weet ik van mijn eigen dat ik vrij perfectionistisch ben ingesteld. Maar dit kwam de afgelopen drie jaar veel meer tot uiting dan ik zou willen. 
+                    Dit bedoel ik vooral op een manier dat dit mij vaak extra tijd heeft gekost om iets klaar te krijgen. Ik wil het altijd zo goed mogelijk doen en verlies mij vaak in kleine details. 
+                    Hierdoor ondervond ik van mijn eigen dat ik soms inefficiënt aan het werken was. Hieraan heb ik de afgelopen jaren leren werken om deze zelf sneller op te merken, wat ook gelukt is. 
+                    Maar er is zeker nog ruimte voor verbetering. 
+                    </p>
+                    <p className="eindreflectie-paragraph">
+                    Op het vlak van de opleiding zelf heb ik heel veel bijgeleerd. Als je kijkt van in het begin van de opleiding, niet deftig weten wat een API was. 
+                    Tot deze nu zelf bouwen, dan besef je wel van over je komt. Daarom ben ik blij dat ik met zoveel verschillende technologieën en tools in aanraking ben gekomen. 
+
+                    </p>
+                    <p className="eindreflectie-paragraph">
+                    Dit alles heeft zich vertaald in een nieuwe baan bij het bedrijf waar ik stage gelopen heb. Hier heb ik kennisgemaakt met integratie ontwikkeling en low code tools. 
+                    Tijdens de stage bleek dat dat mij wel ligt en daarom ben ik dus ook blij dat ik dit kan verderzetten als fulltime baan.
+                    </p>
+                    <p className="eindreflectie-paragraph">
+                    Dit alles was natuurlijk nooit bereikt geweest zonder de steun van mijn omgeving en in het bijzonder mijn vriendin, Ine en sinds kort mijn dochter, Pauline. 
+                    Zij zorgde voor de nodige ontspanning tussen het vele werk door. Waarvoor een dikke merci!
+                    </p>
                 </div> 
             </div>
     </section>
